4e7041db3d3b4ec3564d489d8b7eb3b70bf6c82b
Syrax <syraxrakotonarivo@gmail.com>
2021-12-11 09:15:07 +0300

    add data in token

---
+++ src/Events/JwtCreatedSubscriber.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Events;
+
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JwtCreatedSubscriber{
+    public function updateJwtData(JWTCreatedEvent $event){
+        //get user
+        //add lastname and firstname
+        $user = $event->getUser();
+        $data = $event->getData();
+        $data['lastname'] = $user->getLastname();
+        $data['firstname'] = $user->getFirstname();
+        $event->setData($data);
+    }
+}
